326d906862acc92cd9e5a667a03691125e7d9be8
ali.asad <ali.asad@10pearls.local>
2018-12-05 14:49:55 +0500

    Merge Sort

--- Sorting and Searching/Sorting/Sorting.java
+++ Sorting and Searching/Sorting/Sorting.java
@@ -5,7 +5,13 @@ public class Sorting {
 		StringGenerator sg = new StringGenerator();
 		String str[] = sg.generate(5, 20);
 		
-		String sortedArr[] = SortingUtil.insertionSort( str );
+		for( int k=0 ; k<str.length ; k++ ) {
+			System.out.println( str[k] );
+		}
+		System.out.println( "\n\n\n" );
+		//String sortedArr[] = SortingUtil.insertionSort( str );
+		String sortedArr[] = SortingUtil.mergeSort( str );
+		System.out.println( "\n\n\n" );
 		for( int k=0 ; k<sortedArr.length ; k++ ) {
 			System.out.println( sortedArr[k] );
 		}
--- Sorting and Searching/Sorting/SortingUtil.java
+++ Sorting and Searching/Sorting/SortingUtil.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SortingUtil {
 	
 	public static String[] insertionSort(String unsortedArr[]) {
@@ -17,10 +19,51 @@ public class SortingUtil {
 		return unsortedArr;
 	}
 	
+	public static String[] mergeSort(String unsortedArr[]) {
+		return splitAndMerge(unsortedArr);
+	}
+	
+	public static String[] splitAndMerge(String arr[]) {
+		if( arr.length<2 ) return arr;
+		int mid = arr.length / 2;
+		String upperHalf[] = splitAndMerge( Arrays.copyOfRange(arr, 0, mid) );
+		String lowerHalf[] = splitAndMerge( Arrays.copyOfRange(arr, mid, arr.length) );
+		String merged[] = merge(upperHalf, lowerHalf);
+		return merged;
+	}
+	
+	public static String[] merge(String upperHalf[], String lowerHalf[]) {
+		String sortedArr[] = new String[upperHalf.length + lowerHalf.length];
+		int upperHalfLength = upperHalf.length;
+		int lowerHalfLength = lowerHalf.length;
+		int k=0, i=0, j=0;
+		while( i<upperHalfLength && j<lowerHalfLength ) {
+			if( upperHalf[i].compareTo( lowerHalf[j] ) > 0 ) {
+				sortedArr[k++] = lowerHalf[j++];
+			} else {
+				sortedArr[k++] = upperHalf[i++];
+			}
+		}
+		while( i<upperHalfLength ) {
+			sortedArr[k++] = upperHalf[i++];
+		}
+		while( j<lowerHalfLength ) {
+			sortedArr[k++] = lowerHalf[j++];
+		}
+		return sortedArr;
+	}
+	
 	public static void swap(String arr[], int index1, int index2) {
 		String temp = arr[index1];
 		arr[index1] = arr[index2];
 		arr[index2] = temp;
 	}	
+	
+	
+	public static void printArray(String arr[]) {
+		for( int q=0 ; q<arr.length ; q++ ) {
+			System.out.println( arr[q] );
+		}
+	}
 	
 }
